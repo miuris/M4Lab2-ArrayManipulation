6f8e1408621e27f91c7f17f29c133f524a67d790
Miuris.G <miuris.gutierrez@gmail.com>
2025-07-05 20:10:30 -0400

    Updated functions structure

--- script.js
+++ script.js
@@ -3,14 +3,20 @@ let shoppingList = []
 const removeBtn = document.getElementById("removeBtn");
 const addBtn = document.getElementById("addBtn");
 
-function addItem() {
+function addItem(item) {
+    shoppingList.unshift(item);
 
 }
 
 function removeLastItem() {
+    shoppingList.pop();
 
 }
 
 function displayList() {
     
+}
+
+function filterItems() {
+
 }
